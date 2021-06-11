ae199882a1ab2cc7414db396023afe4cafacfd25
Armand <thillard.armand@gmail.com>
2021-06-11 10:26:17 +0200

    restservice + no strict mode

--- src/app/restservice.service.ts
+++ src/app/restservice.service.ts
@@ -1,12 +1,40 @@
 import { Injectable } from '@angular/core';
-import{ HttpClient} from'@angular/common/http'
-import{ World, Pallier, Product} from'./world';
+import { HttpClient } from '@angular/common/http';
+import { World, Pallier, Product } from './world';
 
 @Injectable({
-  providedIn: 'root'
-  
+  providedIn: 'root',
 })
 export class RestserviceService {
+  private _server = 'http://localhost:8081/';
+  private _user = '';
+  private _http: HttpClient;
 
-  constructor(privatehttp: HttpClient) { }
+  constructor(privatehttp: HttpClient) {
+    this._http = privatehttp;
+  }
+
+  public getWorld(): Promise<World> {
+    return this._http
+      .get(this._server + 'adventuregaragiste/generic/world')
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+
+  public get user(): string {
+    return this._user;
+  }
+
+  public set user(user: string) {
+    this._user = user;
+  }
+
+  public get server(): string {
+    return this._server;
+  }
 }
